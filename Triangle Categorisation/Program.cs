029238aaef34422fead9427948d5aaac722a8e28
atmni5 <aaron.tumini@griffithuni.edu.au>
2017-07-05 14:27:33 +1000

    Online commit

---
+++ Triangle Categorisation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Triangle_Categorisation
+{
+	class Program
+	{
+		Triangle triangle;
+		
+		public static void Main(string[] args)
+		{
+			Program program = new Program();
+			program.Sides();
+			
+		}
+		
+		private void Sides()
+		{
+			
+			double sideA = 0;
+			double sideB = 0;
+			double sideC = 0;
+			string input;
+			
+			while(true){
+			Console.Write("Enter three sides of a triangle. (eg. 3 4 5)\n");
+			input = Console.ReadLine();
+			string[] values = input.Split(' ');
+
+            //Test for integers entered
+			try{
+				Double.TryParse(values[0], out sideA);
+				Double.TryParse(values[1], out sideB);
+				Double.TryParse(values[2], out sideC);	
+			}
+			catch {
+				Console.WriteLine("Please enter three numbers only!\n");
+				continue;
+			}
+
+			triangle = new Triangle(sideA, sideB, sideC);
+			string output = triangle.TriangleType();
+			Console.WriteLine(output);
+			}
+		}
+		
+	}
+}
---
+++ Triangle Categorisation/Triangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Triangle_Categorisation
+{
+
+	public class Triangle
+	{
+		public double sideA;
+		public double sideB;
+		public double sideC;
+		
+		public Triangle(double a, double b, double c)
+		{		
+			this.sideA = a;
+			this.sideB = b;
+			this.sideC = c;
+		}
+		
+		public string TriangleType(){
+			
+			double answer1, answer2, longSide, side1, side2;
+			side1 = side2 = longSide = 0;
+			//If sideA is the largest side
+			if (this.sideA > this.sideB && this.sideA > this.sideC){
+				 longSide = this.sideA;
+				 side1 = this.sideB;
+				 side2 = this.sideC;
+			}
+			//If sideB is the largest side
+			else if (this.sideB > this.sideA && this.sideB > this.sideC ){
+				side1 = this.sideA;
+				longSide = this.sideB;
+				side2 = this.sideC;
+			}
+			//If sideC is the largest side
+			else if (this.sideC > this.sideA && this.sideB < this.sideC ){
+				side1 = this.sideA;
+				side2 = this.sideB;
+				longSide = this.sideC;
+			}
+			else {
+				longSide = this.sideA;
+				side1 = this.sideB;
+				side2 = this.sideC;
+			}
+			
+			//Determine the type of triangle
+            answer1 = Math.Pow(side1, 2) + Math.Pow(side2, 2);
+            answer2 = Math.Pow(longSide, 2);
+            if (answer1 > answer2)
+            {
+                const string output = "The triangle is an acute angle triangle\n";
+                return output;
+            }
+            else if (answer1 < answer2)
+            {
+                const string output = "The triangle is an obtuse angle triangle\n";
+				return output;
+            }
+            else if (answer1.Equals(answer2))
+            {
+                const string output = "The triangle is an right angle triangle\n";
+                return output;
+            }
+            
+			return "";
+		
+		}
+		
+	}
+}
